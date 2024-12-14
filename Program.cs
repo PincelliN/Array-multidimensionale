744bdac9f15a567907762ae351f4485492ae841b
Nicolò <pincelli.n@gmail.com>
2024-12-14 16:04:59 +0100

    loop for

--- Program.cs
+++ Program.cs
@@ -1,2 +1,19 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+int [,] a=new int[5,5];
+
+int value=1;
+
+for (int e = 0; e < 5; e++)
+{
+    for (int i = 0; i <5; i++)
+    {
+        if(i==0){
+          a[e,i]= 0;
+           value++; 
+        }else{
+        a[e,i]= value++;
+            }
+      Console.Write( a[e,i]); 
+    }
+Console.WriteLine();
+}
